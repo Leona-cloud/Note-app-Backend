ec5d0b4c342d10c5e98a5c166a4e779a85a62008
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2021-10-04 18:21:40 +0100

    Add files via upload

---
+++ Model/users.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const PasswordComplexity = require("joi-password-complexity");
+
+
+const userSchema = new mongoose.Schema({
+    userName:{
+        type : String,
+        required: true,
+        unique: true,
+        minlength: 6,
+        maxlength: 12
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase:true
+    },
+    password: new PasswordComplexity({
+        min: 8,
+        max: 25,
+        lowercase: 1,
+        uppercase: 1,
+        numeric: 1,
+        symbol: 1,
+        requirementCount: 4
+    }),
+    confirmPassword: {
+        type: String,
+        required: true,
+         minlength: 8,
+        maxlength: 1024
+    },
+
+});
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, config.get('jwtPrivateKey'));
+    return token;
+}
+
+const User = mongoose.model('User', userSchema);
+
+
+function validateUser(user){
+    const schema = Joi.object({
+        userName: Joi.string().required().min(6).max(12),
+        email: Joi.string().required().trim().lowercase().email(),
+        password: new PasswordComplexity({
+            min: 8,
+            max: 25,
+            lowercase: 1,
+            uppercase: 1,
+            numeric:1,
+            symbol: 1,
+            requirementCount: 4
+        }),
+        confirmPassword: Joi.ref('password'),
+    })
+    .with('password', 'confirmPassword')
+
+    return schema.validate(user)
+};
+
+function loginValidation(user){
+    const logSchema = Joi.object({
+        email: Joi.string().required().trim().lowercase().email(),
+        password: Joi.string().required().min(8).max(12),
+    })
+    return logSchema.validate(user)
+}
+
+
+module.exports.User = User;
+module.exports.validate = validateUser;
+module.exports.loginValidation = loginValidation;
