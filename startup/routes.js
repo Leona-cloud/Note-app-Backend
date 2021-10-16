e32238f3c9643985e572884b13ce20c0f6840b57
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2021-10-16 23:16:57 +0100

    Update routes.js

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const notes = require('../Routes/note');
-const user = require('../Routes/User');
-const login = require('../Routes/login');
+const auth= require('../Routes/auth');
 const error = require('../middleware/error');
 
 
@@ -9,7 +8,6 @@ module.exports = function (app){
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api/notes', notes);
-app.use('/api/auth', login);
-app.use('/api/auth', user);
+app.use('/api/auth', auth);
 app.use(error);
 }
