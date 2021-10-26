57effcd384bf4d7bc9c108040ea64def83470a90
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2021-10-26 21:49:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
+require('./startup/prod')(app);
 
     const port = process.env.PORT || 3000;
     const server = app.listen(port, ()=>{
@@ -14,4 +15,4 @@ require('./startup/config')();
         console.log( `server is running on port ${port}.....`);
     });
 
-module.exports = server;  
+module.exports = server;    
